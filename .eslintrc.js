41ca41d2d62fcb8eaae7405463d52d0999713f54
Fernando Guerrero <devrrior@gmail.com>
2023-03-26 02:24:20 -0600

    chore(project): Set up whole project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,34 @@
 module.exports = {
-	extends: ["eslint-config-codely/typescript"],
-	overrides: [
-		{
-			files: ["*.ts", "*.tsx"],
-			parserOptions: {
-				project: ["./tsconfig.json"],
-			},
-		},
+	env: {
+		es2021: true,
+		jest: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+		"plugin:promise/recommended",
 	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: [
+		"@typescript-eslint",
+		"simple-import-sort",
+		"promise",
+		"prettier",
+		"import",
+	],
+	rules: {
+		"prettier/prettier": ["error", { useTabs: true }],
+		"import/extensions": "off",
+		"simple-import-sort/imports": "error",
+		"simple-import-sort/exports": "error",
+	},
 };
