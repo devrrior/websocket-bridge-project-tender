5120812bcbc40bcd0d9ed0fe12e1b23a2e9460ba
Fernando Guerrero <devrrior@gmail.com>
2023-03-28 10:12:34 -0600

    Revert "fix(project): fix format"
    This reverts commit 9c074104df1c87cab129aeec7f996b244e7bf07b.

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -8,7 +8,7 @@ export interface IUser {
 export const userSchema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	roles: { required: true, type: Array<string> }
+	roles: { required: true, type: Array<string> },
 });
 
 export const User = model("User", userSchema);
