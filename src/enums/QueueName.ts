00216a921c60746961821bbd18f7727874d73b42
Fernando Guerrero <devrrior@gmail.com>
2023-03-29 06:28:33 -0600

    fix(websocket): fix logic event

--- src/enums/QueueName.ts
+++ src/enums/QueueName.ts
@@ -2,7 +2,7 @@ enum QueueName {
 	newProject = "projectTender.project.new",
 	newProjectList = "projectTender.projectList.new",
 	newUser = "projectTender.user.new",
-	currentUser = "projectTender.user.new",
+	currentUser = "projectTender.user.current",
 }
 
 export default QueueName;
