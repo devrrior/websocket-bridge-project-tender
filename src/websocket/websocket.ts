e2c7f42f2d8ed0c19b0ba06683b2cc19fca4cd2d
Fernando Guerrero <devrrior@gmail.com>
2023-03-31 01:40:20 -0600

    fix(cors): remove /

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -30,7 +30,7 @@ const users = new Set();
 const setUpWebsocket = async () => {
 	const io = new Server({
 		cors: {
-			origin: "https://fer.software/",
+			origin: "https://fer.software",
 			methods: ["GET", "POST"],
 		},
 	});
